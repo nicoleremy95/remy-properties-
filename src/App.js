400aec9df07f34d22e68e59c4e182590b0eb69b7
Nicole Remy <nicoleremy95@gmail.com>
2021-02-03 17:34:47 -0800

    Npm i react-router-dom, Add Router to App.js with pages, Add pages to pages folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Appbar from '../src/components/Appbar/Appbar';
+import About from '../src/pages/About/About';
+import People from '../src/pages/People/People';
+import Properties from '../src/pages/Properties/Properties';
+import Contact from '../src/pages/Contact/Contact';
+import Home from '../src/pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Appbar/>
+      <Router>
+        <Appbar/>
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route exact path="/properties">
+            <Properties/>
+          </Route>
+          <Route exact path="/about">
+            <About/>
+          </Route>
+          <Route exact path="/people">
+            <People/>
+          </Route>
+          <Route exact path="/contact">
+            <Contact/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
