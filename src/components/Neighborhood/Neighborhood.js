f8652a9df589f08fe33478849df138580d0471ed
Nicole Remy <nicoleremy95@gmail.com>
2021-02-17 11:43:12 -0800

    Move logic from Neighborhoods page to Neighborhood component

--- src/components/Neighborhood/Neighborhood.js
+++ src/components/Neighborhood/Neighborhood.js
@@ -1,8 +1,98 @@
 import React from 'react'
+import Tacoma from '../../images/Tacoma.jpg';
+import AmericanLakeSquare from '../../images/AmericanLakeSquare.jpg';
+import {makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { Link } from 'react-router-dom';
+import {Container, Grid} from '@material-ui/core/';
 
-export default function Neighborhood() {
+const useStyles = makeStyles((theme)=>({
+    colorText: {
+        color: "#3882A7",
+        textTransform: "uppercase"
+    },
+    text: {
+        marginTop: "200px",
+        paddingRight: '100px'
+    },
+    textRight: {
+        marginTop: "200px",
+        paddingLeft: '100px'
+    },
+    title:{
+        letterSpacing: "2px"
+    },
+    paragraph: {
+        letterSpacing: "2px"
+    },
+    background: {
+        // opacity: .5,
+        filter: 'grayscale(70%) blur(1px)',
+        background: AmericanLakeSquare
+        // backgroundImage: "linear-gradient(180deg,rgba(18,18,18,0.5) 0%,#121212 100%)"
+    },
+    overlay: {
+        position: 'relative',
+        maxHeight: '500px',
+        bottom: 700,
+        right: 100,
+
+    },
+    backgroundRight: {
+        // opacity: .5,
+        width: '100%',
+        filter: 'grayscale(70%) blur(1px)',
+        background: AmericanLakeSquare
+        // backgroundImage: "linear-gradient(180deg,rgba(18,18,18,0.5) 0%,#121212 100%)"
+    },
+    overlayRight: {
+        position: 'relative',
+        height: '500px',
+        bottom: 600,
+        left: 300,
+
+    }
+}));
+
+export default function Neighborhood(props) {
+    const classes = useStyles();
+
     return (
         <div>
+            {props.boolean? 
+            <Grid container>
+                <Grid item lg={6}>
+                    <Container maxWidth="sm">
+                        <div className={classes.text}>
+                            <h4 className={classes.colorText}>view the extraordinary</h4>
+                            <h1 className={classes.title}>Homes in {props.name}</h1>
+                            <p className={classes.paragraph}>{props.description}</p>
+                            <Button variant="contained"><Link>view our properties</Link></Button>
+                        </div>
+                    </Container>
+                </Grid>
+                <Grid item lg={6}>
+                    <img className={classes.background} src={props.image} alt={props.name}/>
+                    <img className={classes.overlay} src={props.image} alt={classes.name}/>
+                </Grid>
+            </Grid> 
+            :
+            <Grid container>
+                <Grid item lg={6}>
+                    <img className={classes.backgroundRight} src={props.image} alt={props.name}/>
+                    <img className={classes.overlayRight} src={props.image} alt={props.name}/>
+                </Grid>
+                <Grid item lg={6}>
+                    <Container maxWidth="sm">
+                    <div className={classes.textRight}>
+                            <h4 className={classes.colorText}>view the extraordinary</h4>
+                            <h1 className={classes.title}>Homes in {props.name}</h1>
+                            <p className={classes.paragraph}> {props.description}</p>
+                            <Button variant="contained"><Link>view our properties</Link></Button>
+                        </div>
+                    </Container>
+                </Grid>
+            </Grid>   }
             
         </div>
     )
--- src/pages/Neighborhoods/Neighborhoods.js
+++ src/pages/Neighborhoods/Neighborhoods.js
@@ -4,55 +4,26 @@ import AmericanLakeSquare from '../../images/AmericanLakeSquare.jpg';
 import {makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-import Tacoma from '../../images/Tacoma.jpg'
+import Tacoma from '../../images/Tacoma.jpg';
+import Neighborhood from '../../components/Neighborhood/Neighborhood';
 
+const neighborhoodArr = [
+    {
+        name: "lakewood",
+        boolean: true,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: AmericanLakeSquare
+    },
+    {
+        name: "tacoma",
+        boolean: false,
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: Tacoma
+    },
+]
 
 const useStyles = makeStyles((theme)=>({
-    colorText: {
-        color: "#3882A7",
-        textTransform: "uppercase"
-    },
-    text: {
-        marginTop: "200px",
-        paddingRight: '100px'
-    },
-    textRight: {
-        marginTop: "200px",
-        paddingLeft: '100px'
-    },
-    title:{
-        letterSpacing: "2px"
-    },
-    paragraph: {
-        letterSpacing: "2px"
-    },
-    background: {
-        // opacity: .5,
-        filter: 'grayscale(70%) blur(1px)',
-        background: AmericanLakeSquare
-        // backgroundImage: "linear-gradient(180deg,rgba(18,18,18,0.5) 0%,#121212 100%)"
-    },
-    overlay: {
-        position: 'relative',
-        maxHeight: '500px',
-        bottom: 700,
-        right: 100,
-
-    },
-    backgroundRight: {
-        // opacity: .5,
-        width: '100%',
-        filter: 'grayscale(70%) blur(1px)',
-        background: AmericanLakeSquare
-        // backgroundImage: "linear-gradient(180deg,rgba(18,18,18,0.5) 0%,#121212 100%)"
-    },
-    overlayRight: {
-        position: 'relative',
-        height: '500px',
-        bottom: 600,
-        left: 300,
-
-    }
+    
 }));
 
 export default function Neighborhoods() {
@@ -60,41 +31,14 @@ export default function Neighborhoods() {
 
     return (
         <div>
-            <Grid container>
-                <Grid item lg={6}>
-                    <Container maxWidth="sm">
-                        <div className={classes.text}>
-                            <h4 className={classes.colorText}>view the extraordinary</h4>
-                            <h1 className={classes.title}>Homes in Lakewood</h1>
-                            <p className={classes.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <Button variant="contained"><Link>view our properties</Link></Button>
-                        </div>
-                    </Container>
-                </Grid>
-                <Grid item lg={6}>
-                    {/* <div className={classes.background}></div> */}
-                    <img className={classes.background} src={AmericanLakeSquare} alt="American Lake"/>
-                    <img className={classes.overlay} src={AmericanLakeSquare} alt="American Lake"/>
-                </Grid>
-            </Grid> 
-            <Grid container>
-                <Grid item lg={6}>
-                    {/* <div className={classes.background}></div> */}
-                    <img className={classes.backgroundRight} src={Tacoma} alt="American Lake"/>
-                    <img className={classes.overlayRight} src={Tacoma} alt="American Lake"/>
-                </Grid>
-                <Grid item lg={6}>
-                    <Container maxWidth="sm">
-                    <div className={classes.textRight}>
-                            <h4 className={classes.colorText}>view the extraordinary</h4>
-                            <h1 className={classes.title}>Homes in Tacoma</h1>
-                            <p className={classes.paragraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            <Button variant="contained"><Link>view our properties</Link></Button>
-                        </div>
-                    </Container>
-                </Grid>
-            </Grid> 
-            
+            {neighborhoodArr.map(place=>(
+                <Neighborhood
+                    name={place.name}
+                    description={place.description}
+                    boolean={place.boolean}
+                    image={place.image}
+                />
+            ))}    
         </div>
     )
 }
